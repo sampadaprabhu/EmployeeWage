50563c501be3db1d29f415b6b8a1be868ac527d4
sampadaprabhu <sampadaprabhu98@gmail.com>
2020-01-21 16:34:29 +0530

    REFACTORED: Use Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,19 +2,23 @@
 WAGE_PER_HOUR=20
 FULL_DAY_HOUR=8
 PART_TIME_HOUR=4
+isFullTimePresent=1
+isPartTimePresent=2
+isAbsent=0
 echo "Welcome To Employee Wage"
 random=$((RANDOM%3))
-if (( $random == 1 ))
-then
-		echo "Full Time Employee is Present"
-		Daily_Employee_Wage=$(( $WAGE_PER_HOUR * $FULL_DAY_HOUR ))
-		echo "Daily Employee Wage is: $Daily_Employee_Wage"
-elif (( $random == 2 ))
-then
-		echo "Part Time Employee is Present"
-		Part_Time_Employee_Daily_Wage=$(( $WAGE_PER_HOUR * $PART_TIME_HOUR ))
-		echo "Part Time Employee Daily Wage is: $Part_Time_Employee_Daily_Wage"
-else
-	echo "Employee is Absent"
-fi
+case $random in
+				$isFullTimePresent)
+						echo "Full Time Employee is Present"
+						Daily_Employee_Wage=$(( $WAGE_PER_HOUR * $FULL_DAY_HOUR ))
+						echo "Daily Employee Wage is: $Daily_Employee_Wage"
+						;;
+				$isPartTimePresent)
+						echo "Part Time Employee is Present"
+						Part_Time_Employee_Daily_Wage=$(( $WAGE_PER_HOUR * $PART_TIME_HOUR ))
+						echo "Part Time Employee Daily Wage is: $Part_Time_Employee_Daily_Wage"
+						;;
+				$isAbsent)
+						echo "Employee is Absent"
+esac
 
