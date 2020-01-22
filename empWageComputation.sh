5ee4a1b5390b90c942acbbc40f47d7aa2a8945a7
sampadaprabhu <58414826+sampadaprabhu@users.noreply.github.com>
2020-01-22 10:41:36 +0530

    ADDED:Calculate Daily Wage Using Array

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -9,12 +9,14 @@ TOTAL_HOURS=100
 TOTAL_DAYS=20
 hours=0
 days=0
+i=0
 One_Day_Hours=0
 echo "Welcome To Employee Wage"
 getWorkingHours()
 {
 	while (( $TOTAL_DAYS >= $days && $TOTAL_HOURS >= $hours ))
 	do
+		((i++))
 		random=$((RANDOM%3))
 		((days++))
 		case $random in
@@ -27,7 +29,7 @@ getWorkingHours()
 						hours=$(( $hours + $PART_TIME_HOUR ))
 						;;
 		esac
-		dailyWage[$days]=$(($WAGE_PER_HOUR * $One_Day_Hours))
+		dailyWage[$i]=$(($WAGE_PER_HOUR * $One_Day_Hours))
 	done
 }
 getWorkingHours
